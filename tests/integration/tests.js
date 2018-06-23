d7268a349c529d3d442981a42f66bab6030dfdfc
Ruaridh <ruaridhdunbar@gmail.com>
2018-06-23 17:34:26 +0100

    passes tests to check calculator can concatenate multiple button clicks, and that can clear runningtotal mid calculation without affecting the result

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -75,40 +75,49 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('0')
   })
 
+  it('should concatenate multiple button clicks', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('123')
+  })
+
   it('should have working operator buttons - operator +', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number2')).click();
+    element(by.css('#number1')).click();
     element(by.css('#operator_add')).click();
-    element(by.css('#number2')).click();
+    element(by.css('#number4')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
   })
 
   it('should have working operator buttons - operator -', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number4')).click();
+    element(by.css('#number7')).click();
     element(by.css('#operator_subtract')).click();
-    element(by.css('#number2')).click();
+    element(by.css('#number4')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
   })
 
   it('should have working operator buttons - operator /', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number8')).click();
-    element(by.css('#operator_divide')).click();
     element(by.css('#number2')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number7')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
   })
 
   it('should have working operator buttons - operator *', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number8')).click();
+    element(by.css('#number3')).click();
     element(by.css('#operator_multiply')).click();
-    element(by.css('#number2')).click();
+    element(by.css('#number5')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('16')
+    expect(running_total.getAttribute('value')).to.eventually.equal('15')
   })
 
   it('should chain multiple operations together', function(){
@@ -124,6 +133,17 @@ describe('calculator functionality', function() {
     element(by.css('#number9')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  })
+
+  it('should clear runningTotal without affecting calculation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#clear')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('7')
   })
 
   it('should work as intended for negative numbers', function(){
@@ -171,7 +191,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('Infinity')
   })
 
-  
+
 
 
 
